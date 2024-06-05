60c48762b7b7c7483a0cfcd9a8c0bc4a75e27b1e
norninha do grau <106700570+moyses17@users.noreply.github.com>
2024-06-05 15:25:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -44,11 +44,18 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
-function mostraPergunta() {
-    perguntaAtual = perguntas[atual];
-}
+
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
 }
-mostraPergunta()
+function mostraAlternativas() {
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativa.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostraPergunta();
